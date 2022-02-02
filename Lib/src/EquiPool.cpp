cf5071bfe484363941380abd584d19c3c69d2910
Andrea Casalino <andrecasa91@gmail.com>
2022-02-02 23:05:02 +0100

    refactoring

--- Lib/src/EquiPool.cpp
+++ Lib/src/EquiPool.cpp
@@ -1,40 +1,44 @@
 /**
-* Author:    Andrea Casalino
-* Created:   25.09.2020
-*
-* report any bug to andrecasa91@gmail.com.
-**/
+ * Author:    Andrea Casalino
+ * Created:   25.09.2020
+ *
+ * report any bug to andrecasa91@gmail.com.
+ **/
 
-#include "../header/EquiPool.h"
 #include "QueueStrategy.h"
+#include <EquiPool.h>
 
-namespace thpl::equi{
+#include <list>
 
-    class Pool::EquiQueue : public IPool::QueueStrategy {
-    public:
-        inline bool isEmpty() override{ return this->queue.empty(); };
+namespace thpl::equi {
 
-        inline std::function<void(void)> pop() override{
-            std::function<void(void)> temp = std::move(this->queue.front());
-            this->queue.pop_front();
-            return temp;
-        };
+class Pool::EquiQueue : public IPool::QueueStrategy {
+public:
+  bool isEmpty() const override { return this->queue.empty(); };
 
-        inline void push(const std::function<void(void)>& task){ this->queue.push_back(task); };
-    private:
-        std::list<std::function<void(void)>> queue;
-    };
+  std::function<void(void)> pop() override {
+    std::function<void(void)> temp = std::move(this->queue.front());
+    this->queue.pop_front();
+    return temp;
+  };
 
-    Pool::Pool(const std::size_t& poolSize)
-        : IPool(poolSize, std::make_unique<Pool::EquiQueue>()) {
-    }
+  void push(std::function<void(void)> task) {
+    this->queue.emplace_back(std::move(task));
+  };
 
-    void Pool::push(const std::function<void(void)>& newTask){
-        {
-            std::lock_guard<std::mutex> lk(this->queueMtx);
-            static_cast<EquiQueue*>(this->queue.get())->push(newTask);
-        }
-        ++this->remainingTasks;
-    }
+private:
+  std::list<std::function<void(void)>> queue;
+};
 
+Pool::Pool(const std::size_t &poolSize)
+    : IPool(poolSize, std::make_unique<Pool::EquiQueue>()) {}
+
+void Pool::push(std::function<void(void)> newTask) {
+  {
+    std::scoped_lock<std::mutex> lk(this->queueMtx);
+    static_cast<EquiQueue *>(this->queue.get())->push(std::move(newTask));
+  }
+  ++this->remainingTasks;
 }
+
+} // namespace thpl::equi
--- Lib/src/PrioritizedPool.cpp
+++ Lib/src/PrioritizedPool.cpp
@@ -1,49 +1,54 @@
 /**
-* Author:    Andrea Casalino
-* Created:   25.09.2020
-*
-* report any bug to andrecasa91@gmail.com.
-**/
+ * Author:    Andrea Casalino
+ * Created:   25.09.2020
+ *
+ * report any bug to andrecasa91@gmail.com.
+ **/
 
-#include "../header/PrioritizedPool.h"
 #include "QueueStrategy.h"
+#include <PrioritizedPool.h>
 #include <queue>
 #include <utility>
 
-namespace thpl::prty{
+namespace thpl::prty {
 
-    class Pool::PriorityQueue : public IPool::QueueStrategy {
-    public:
-        PriorityQueue();
+class Pool::PriorityQueue : public IPool::QueueStrategy {
+public:
+  PriorityQueue();
 
-        inline bool isEmpty() override{ return this->queue.empty(); };
+  bool isEmpty() const override { return this->queue.empty(); };
 
-        inline std::function<void(void)> pop() override{
-            std::function<void(void)> temp = std::move(this->queue.top().second);
-            this->queue.pop();
-            return temp;
-        };
+  std::function<void(void)> pop() override {
+    std::function<void(void)> temp = std::move(this->queue.top().second);
+    this->queue.pop();
+    return temp;
+  };
 
-        inline void push(const std::function<void(void)>& task, const unsigned int& pr) { this->queue.push(std::make_pair(pr, task)); };
-    private:
-        typedef std::pair<unsigned int, std::function<void(void)>> task;
-        std::priority_queue< task, std::vector<task>, std::function<bool(const task&, const task&)>> queue;
-    };
+  void push(std::function<void(void)> task, const unsigned int &pr) {
+    this->queue.emplace(std::make_pair(pr, std::move(task)));
+  };
 
-    Pool::PriorityQueue::PriorityQueue()
-        : queue([](const task& a, const task& b){ return a.first < b.first; }) {
-    };
+private:
+  using Task = std::pair<unsigned int, std::function<void(void)>>;
+  std::priority_queue<Task, std::vector<Task>,
+                      std::function<bool(const Task &, const Task &)>>
+      queue;
+};
 
-    Pool::Pool(const std::size_t& poolSize)
-        : IPool(poolSize, std::make_unique<PriorityQueue>()) {
-    }
+Pool::PriorityQueue::PriorityQueue()
+    : queue([](const Task &a, const Task &b) { return a.first < b.first; }){};
 
-    void Pool::push(const std::function<void(void)>& newTask, const unsigned int& priority){
-        {
-            std::lock_guard<std::mutex> lk(this->queueMtx);
-            static_cast<PriorityQueue*>(this->queue.get())->push(newTask, priority);
-        }
-        ++this->remainingTasks;
-    }
+Pool::Pool(const std::size_t &poolSize)
+    : IPool(poolSize, std::make_unique<PriorityQueue>()) {}
 
+void Pool::push(std::function<void(void)> newTask,
+                const unsigned int priority) {
+  {
+    std::scoped_lock<std::mutex> lk(this->queueMtx);
+    static_cast<PriorityQueue *>(this->queue.get())
+        ->push(std::move(newTask), priority);
+  }
+  ++this->remainingTasks;
 }
+
+} // namespace thpl::prty
